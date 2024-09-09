293c64692153f3924c8fc1d8dc6b3ed35851e90d
arielmoren-hub <arielmoren4@gmail.com>
2024-09-09 13:33:38 -0300

    se agrego la carpeta domain

---
+++ domain/entities/alpargata.tsx
@@ -0,0 +1,18 @@
+export interface Alpargata {
+    id?: string;
+    talle: number;
+    colorNegro: number;
+    colorAzul: number;
+    colorVerde: number;
+    colorBordo: number;
+}
+
+export const defaultAlpargata = {
+    id: "",
+    talle: 0,
+    colorNegro: 0,
+    colorAzul: 0,
+    colorVerde: 0,
+    colorBordo: 0,
+};
+
---
+++ domain/useCases/Alpargatas.tsx
@@ -0,0 +1,15 @@
+import { Alpargata } from "../entities/alpargata";
+
+// const { singIn } = new AuthRepositoryImpl();
+// export const singInAuthUseCase = async (user : UserSignIn ) => {
+    // return await singIn(user);
+// }
+
+
+export function findAlpargataTalle(alpargata : Alpargata){
+
+}
+
+export function findAlpargataId(id : string){
+
+}
